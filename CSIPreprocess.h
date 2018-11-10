26809e8dc21816c2cb930220d007e17a8dc07b75
Zhiping Jiang <jiangzp.cs@gmail.com>
2018-11-10 16:41:28 +0800

    fix building bug

---
+++ CSIPreprocess.h
@@ -0,0 +1,20 @@
+//
+// Created by 蒋志平 on 2018/11/10.
+//
+
+#ifndef PICOSCENES_PLATFROM_CSI_PREPROCESS_HXX
+#define PICOSCENES_PLATFROM_CSI_PREPROCESS_HXX
+
+#include <cstring>
+#include <cerrno>
+#include <vector>
+#include <array>
+#include <cassert>
+#include <thread>
+#include <future>
+#include <complex>
+#include <deque>
+
+int phaseUnwrapAroundDC(const std::complex<double> csi_matrix[], double mag[], double phase[], int nrx, int ntx, int num_tones);
+
+#endif //PICOSCENES_PLATFROM_CSI_PREPROCESS_HXX
