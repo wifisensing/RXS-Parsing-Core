9feb3b744b50c9d91dd9a3e0900790be51177d7d
Zhiping Jiang <jiangzp.cs@gmail.com>
2018-11-10 16:46:04 +0800

    major update to rxs_enhanced

--- rxs_enhanced.h
+++ rxs_enhanced.h
@@ -12,6 +12,9 @@
 #include <cstdint>
 #include <complex>
 #include <bitset>
+#include "RXSExtraInfo.h"
+#include "CSIMatrixParser.h"
+#include "CSIPreprocess.h"
 
 #define TONE_20M                56 ///< Ath9k NIC returns 56 subcarrier in 20MHz bandwidth
 #define TONE_40M                114 ///< Ath9k NIC returns 114 subcarrier in 40MHz bandwidth
@@ -23,184 +26,10 @@
 #define BITS_PER_COMPLEX_SYMBOL (2 * BITS_PER_SYMBOL)
 #define BITS_PER_SYMBOL         10
 
-#define AR_CSI_EXTRAINFO_HASLENGTH                 0x00000001
-#define AR_CSI_EXTRAINFO_HASVERSION                0x00000002
-#define AR_CSI_EXTRAINFO_HASMACCUR                 0x00000004
-#define AR_CSI_EXTRAINFO_HASMACROM                 0x00000008
-#define AR_CSI_EXTRAINFO_HASCHANSEL                0x00000010
-#define AR_CSI_EXTRAINFO_HASBMODE                  0x00000020
-#define AR_CSI_EXTRAINFO_HASEVM                    0x00000040
-#define AR_CSI_EXTRAINFO_HASTXCHAINMASK            0x00000080
-#define AR_CSI_EXTRAINFO_HASRXCHAINMASK            0x00000100
-#define AR_CSI_EXTRAINFO_HASTXPOWER                0x00000200
-#define AR_CSI_EXTRAINFO_HASCF                     0x00000400
-#define AR_CSI_EXTRAINFO_HASTXTSF                  0x00000800
-#define AR_CSI_EXTRAINFO_HASLASTHWTXTSF            0x00001000
-#define AR_CSI_EXTRAINFO_HASCHANNELFLAGS           0x00002000
-#define AR_CSI_EXTRAINFO_HASRXNESS                 0x00004000
-#define AR_CSI_EXTRAINFO_HASTUNINGPOLICY           0x00008000
-#define AR_CSI_EXTRAINFO_HASPLLRATE                0x00010000
-#define AR_CSI_EXTRAINFO_HASPLLREFDIV              0x00020000
-#define AR_CSI_EXTRAINFO_HASPLLCLKSEL              0x00040000
-
 /**
- * Test the presence of version field.
- * @param featureCode The 32-bit feature code
- * @return true for the presence, and false for not.
+ * Shared RxStatus basic information for Qualcomm Atheros AR9300 series and Intel 5300
  */
-inline bool extraInfoHasVersion(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 0 & 0x1);
-}
-
-/**
- * Test the presence of length field
- *
- * Please note: in the ExtraInfo struct, the length value does not include the length field itself.
- *
- * @param featureCode the 32-bit feature code
- * @return true for the presence, and false for not.
- */
-inline bool extraInfoHasLength(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 1 & 0x1);
-}
-
-/**
- * Test the presence of "current MAC Address" field
- *
- * The "current" MAC address is the mac address attached to the working interface, this MAC address can be modified using bash commands.
- *
- * @param featureCode the 32-bit feature code
- * @return true for the presence, and false for not.
- *
- * @see extraInfoHasMacAddress_Rom
- */
-inline bool extraInfoHasMacAddress_Current(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 2 & 0x1);
-}
-
-/**
- * Test the presence of "Permenant MAC Address" field
- *
- * "Permenant MAC Address" is the mac address store the in the H/W's EEPROM, which cannot be modified.
- *
- * @param featureCode the 32-bit feature code
- * @return true for the presence, and false for not.
- *
- * @see extraInfoHasMacAddress_Current
- */
-inline bool extraInfoHasMacAddress_Rom(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 3 & 0x1);
-}
-
-/**
- * Test the presence of "Channel Selection" field
- *
- * Chansel value is used in Ath9k driver to configure the carrier frequency
- *
- * @param featureCode the 32-bit feature code
- * @return true for the presence, and false for not.
- *
- * @see extraInfoHasBMode
- */
-inline bool extraInfoHasChansel(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 4 & 0x1);
-}
-
-/**
- * Test the presence of "Bmode" field
- *
- * Bmode value is used together with Chansel value in Ath9k driver to configure the carrier frequency.
- *
- * 1 for 2.4GHz band, 0 for 5GHz band.
- *
- * @param featureCode the 32-bit feature code
- * @return true for the presence, and false for not.
- *
- * @see extraInfoHasChansel
- */
-inline bool extraInfoHasBMode(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 5 & 0x1);
-/**
- * Test the presence of EVM data
- *
- * @param featureCode the 32-bit feature code
- * @return true for the presence, and false for not.
- */
-}
-inline bool extraInfoHasEVM(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 6 & 0x1);
-}
-
-/**
- * Test the presence of Tx ChainMask
- *
- * Tx/Rx chainmask is a 3-bit value. Each bit stands for a radio chain.
- * 1 for first chain, 4 for last chain.
- * The chainmask bits combination MUST be in low to high order.
- *
- * @param featureCode the 32-bit feature code
- * @return true for the presence, and false for not.
- */
-inline bool extraInfoHasTxChainMask(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 7 & 0x1);
-}
-inline bool extraInfoHasRxChainMask(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 8 & 0x1);
-}
-inline bool extraInfoHasTxPower(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 9 & 0x1);
-}
-inline bool extraInfoHasCF(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 10 & 0x1);
-}
-inline bool extraInfoHasTxTSF(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 11 & 0x1);
-}
-inline bool extraInfoHasLastHWTxTSF(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 12 & 0x1);
-}
-inline bool extraInfoHasChannelFlags(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 13 & 0x1);
-}
-inline bool extraInfoHasRxNESS(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 14 & 0x1);
-}
-inline bool extraInfoHasTuningPolicy(uint32_t featureCode) {
-    return static_cast<bool>(featureCode >> 15 & 0x1);
-}
-inline bool extraInfoHasPLLRate(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 16 & 0x1);
-}
-inline bool extraInfoHasPLLRefDiv(uint32_t featureCode) {
-    return static_cast<bool>(featureCode >> 17 & 0x1);
-}
-inline bool extraInfoHasPLLClkSel(uint32_t featureCode) {
-	return static_cast<bool>(featureCode >> 18 & 0x1);
-}
-
-enum RXSParsingLevel: uint8_t {
-    BASIC_NOEXTRA_NOCSI = 10,
-    EXTRA_NOCSI,
-    EXTRA_CSI,
-    EXTRA_CSI_UNWRAP
-};
-
-enum ChannelMode : uint8_t {
-    HT20 = 8,
-    HT40_MINUS = 24,
-    HT40_PLUS = 40,
-};
-
-enum AtherosCFTuningPolicy: uint8_t {
-    CFTuningByChansel = 30,
-    CFTuningByFastCC,
-    CFTuningByHardwareReset,
-};
-
-/**
- * The Rx Status Basic Informations
- */
-struct ath_rx_status_basic {
+struct rx_status_basic {
     uint64_t   tstamp;      ///< h/w assigned RX timestamp
     uint16_t   csi_len;     ///< csi length
     uint16_t   channel;     ///< receiving channel frequency, unit is MHz, e.g, 2437
@@ -271,65 +100,6 @@ struct ieee80211_packet_header {
     struct ieee80211_packet_header_info header_info;
 } __attribute__ ((__packed__));
 
-/**
- The mininum set of ExtraInfo used for Tx fabrication.
- 
- @see ExtraInfo
- */
-struct TxExtraInfoMinSet {
-    uint32_t txExtraInfoFeatureCode;
-    uint16_t txExtraInfoLength;
-    uint8_t txExtraInfoVersion;
-    uint8_t txExtraInfoMacAddr_cur[6];
-    uint8_t txExtraInfoMacAddr_rom[6];
-
-    int getTxTSFPos();
-} __attribute__ ((__packed__));
-
-struct ExtraInfo {
-    bool hasLength;
-    bool hasVersion;
-    bool hasMacAddr_cur;
-    bool hasMacAddr_rom;
-    bool hasChansel;
-    bool hasBMode;
-    bool hasEVM;
-    bool hasTxChainMask;
-    bool hasRxChainMask;
-    bool hasTxpower;
-    bool hasCF;
-    bool hasTxTSF;
-	bool hasLastHWTxTSF;
-    bool hasChannelFlags;
-	bool hasRxNess;
-	bool hasTuningPolicy;
-    bool hasPLLRate;
-    bool hasPLLClkSel;
-    bool hasPLLRefDiv;
-    uint16_t length;
-    uint8_t version;
-    uint8_t macaddr_rom[6];
-    uint8_t macaddr_cur[6];
-    uint32_t chansel;
-    uint8_t bmode;
-    int8_t  evm[18];
-    uint8_t txChainMask;
-    uint8_t rxChainMask;
-    uint8_t txpower;
-    uint64_t cf;
-    uint32_t txTSF;
-	uint32_t lastHwTxTSF;
-    uint16_t channelFlags;
-	uint8_t rx_ness;
-	uint8_t tuningPolicy;
-    uint16_t pll_rate;
-    uint8_t pll_clock_select;
-    uint8_t pll_refdiv;
-
-    static int fromBinary(const uint8_t *extraInfoPtr, struct ExtraInfo * extraInfo, uint32_t suppliedFeatureCode = 0);
-    static int toBinary(void * extraInfoPtr);
-};
-
 struct EchoProbeInfo {
 	uint16_t length;
 	uint8_t  version;
@@ -349,7 +119,8 @@ struct EchoProbeInfo {
 } __attribute__ ((__packed__));
 
 struct RXS_enhanced {
-    struct ath_rx_status_basic rxs_basic;
+	bool isAR9300;
+    struct rx_status_basic rxs_basic;
 	uint16_t   csi_pos;     /* CSI data position in raw buffer, used for re-parsing.*/
 	uint16_t   payloadLength;
     uint16_t   rawBufferLength;
@@ -366,48 +137,6 @@ struct RXS_enhanced {
 	RXSParsingLevel parsingLevel;
 };
 
-inline ChannelMode channelFlags2ChannelMode(uint16_t channelFlags) {
-    std::bitset<16> channelFlagSet(channelFlags);
-
-    if  (channelFlagSet.test(3) && channelFlagSet.test(4)) {
-        return HT40_PLUS;
-    }
-
-    if  (channelFlagSet.test(3) && channelFlagSet.test(5)) {
-        return HT40_MINUS;
-    }
-
-    if  (channelFlagSet.test(3)) {
-        return HT20;
-    }
-
-    return HT20;
-}
-
-inline std::string channelModel2String(ChannelMode mode) {
-    switch (mode) {
-        case HT40_PLUS:
-            return "HT40_PLUS";
-        case HT40_MINUS:
-            return "HT40_MINUS";
-        case HT20:
-            return "HT20";
-    }
-}
-
-inline std::string TuningPolicy2String(uint8_t policy) {
-    switch (policy) {
-        case CFTuningByChansel:
-            return "Chansel";
-        case CFTuningByFastCC:
-            return "FastCC";
-        case CFTuningByHardwareReset:
-            return "Reset";
-        default:
-            throw std::runtime_error("[RXS_Enhanced.h] Unknown Tuning Policy for value: " + std::to_string(policy));
-    }
-}
-
 /**
  * calculate the over-the-air Tx duration for a packet
  * @param length packet length
@@ -418,13 +147,6 @@ inline std::string TuningPolicy2String(uint8_t policy) {
  * @return over-the-air tx duration
  */
 uint16_t pkt_duration(uint16_t length, uint8_t mcs, bool wide40BW, bool usingSGI = false, bool lengthWithoutFCS = true);
-
-/**
- * Parse 32-bit feature code into has* values of struct ExtraInfo
- * @param featureCode the input 32-bit feature code
- * @param extraInfo The ExtraInfo to be modified to reflect the feature code
- */
-void featureCodeInterpretation(uint32_t featureCode, struct ExtraInfo * extraInfo);
 
 /**
  * Parse the raw RxS data
@@ -465,18 +187,6 @@ int parse_rxs_enhanced(const uint8_t * inBytes, struct RXS_enhanced *rxs, enum R
  * @return 0 if success
  */
 int reparseCSIMatrixInRXS(struct RXS_enhanced *rxs);
-
-/**
- * Directly inject (in-place add) an ExtraInfo Item into the raw RxS data.
- *
- * The transparency to the upper RxS parsing process makes this method very useful in case of adding some platform-calculated value into RXS struct.
- *
- * @param rxs_raw the raw rxs data
- * @param featureCode_added the feature code for the adding value
- * @param data pointer to the value to be added
- * @param length value length
- */
-void inplaceAddRxExtraInfo(uint8_t *rxs_raw, uint32_t featureCode_added, uint8_t * data, int length);
 
 void inplaceAddTxChronosData(RXS_enhanced * rxs, uint8_t *dataBuffer, int bufferLength);
 
---
+++ rxs_enhanced_parsing.cxx
@@ -0,0 +1,155 @@
+#include "rxs_enhanced.h"
+#ifdef BUILD_WITH_MEX
+    #include "mex.h"
+#endif
+
+uint16_t pkt_duration(uint16_t length, uint8_t mcs, bool wide40BW, bool usingSGI, bool lengthWithoutFCS) {
+    static const auto L_STF = 8;
+    static const auto L_LTF = 8;
+    static const auto L_SIG = 4;
+    static const auto HT_SIG = 8;
+    static const auto HT_STF = 4;
+    static const auto HT_LTF = 4;
+    static const auto OFDM_PLCP_BITS = 22;
+    static const auto LGI = 4.0;
+    static const auto SGI = 3.6;
+
+    static const uint16_t bits_per_symbol[][2] = {
+            /* 20MHz 40MHz */
+            {    26,   54 },     /*  0: BPSK */
+            {    52,  108 },     /*  1: QPSK 1/2 */
+            {    78,  162 },     /*  2: QPSK 3/4 */
+            {   104,  216 },     /*  3: 16-QAM 1/2 */
+            {   156,  324 },     /*  4: 16-QAM 3/4 */
+            {   208,  432 },     /*  5: 64-QAM 2/3 */
+            {   234,  486 },     /*  6: 64-QAM 3/4 */
+            {   260,  540 },     /*  7: 64-QAM 5/6 */
+    };
+//
+    auto streams = mcs / 8 + 1;
+    auto bits = length * 8 + OFDM_PLCP_BITS;
+    if (lengthWithoutFCS)
+        bits += 32;
+    auto symbolbits = bits_per_symbol[mcs % 8][wide40BW] * streams;
+    auto symbols = (bits + symbolbits -1) / symbolbits;
+    auto pktDuration = (usingSGI ? SGI: LGI) * symbols;
+
+    return L_STF + L_LTF + L_SIG + HT_SIG + HT_STF + HT_LTF*streams + pktDuration;
+}
+
+struct RXS_enhanced parseRXS(const uint8_t * inBytes, enum RXSParsingLevel parsingLevel) {
+    struct RXS_enhanced rxs;
+    parse_rxs_enhanced(inBytes, &rxs, parsingLevel);
+    return rxs;
+}
+
+int parse_rxs_enhanced(const uint8_t * inBytes, struct RXS_enhanced *rxs, enum RXSParsingLevel parsingLevel) {
+    uint16_t totalLength = 2;
+    uint32_t magicValue = 0;
+    uint32_t rxFeatureCode=0;
+    uint32_t pos = 0;
+
+    totalLength += *((uint16_t *)(inBytes+pos)); pos += 2;
+    magicValue  = *((uint32_t *)(inBytes+pos)); pos += 4;
+    assert(magicValue == 0x20150315 || magicValue == 0x20120930);
+    rxs->isAR9300 = magicValue == 0x20150315 ? true : false;
+
+    rxFeatureCode = *((uint32_t *)(inBytes+pos)); pos += 4;
+    rxs->rxs_basic = *((struct rx_status_basic *)(inBytes + pos)); pos += sizeof (struct rx_status_basic);
+    if(rxs->rxs_basic.nrx <=0 || rxs->rxs_basic.nrx > 3 || rxs->rxs_basic.ntx <=0 || rxs->rxs_basic.ntx >3 || (rxs->rxs_basic.num_tones != TONE_20M && rxs->rxs_basic.num_tones != TONE_40M) ) {
+        printf("RXS Parser: Impossible values in nrx (%d), ntx (%d), or num_tones (%d). Error occurs in file format or parsing.\n", rxs->rxs_basic.nrx, rxs->rxs_basic.ntx, rxs->rxs_basic.num_tones);
+        return 1;
+    }
+
+    ExtraInfo::fromBinary(inBytes+pos, &rxs->rxExtraInfo, rxFeatureCode); pos += rxs->rxExtraInfo.length + 2;
+    if (rxs->rxExtraInfo.hasEVM) {
+        for (auto i = 0 ; i < 18; i++) {
+            rxs->rxExtraInfo.evm[i] += rxs->rxs_basic.noise;
+            if (rxs->rxExtraInfo.evm[i] > 0) {
+                rxs->rxExtraInfo.evm[i] = -128;
+            }
+        }
+    }
+
+    rxs->csi_pos = pos;
+    if (parsingLevel >= EXTRA_CSI ) {
+        if (rxs->isAR9300)
+            ar_parse_csi_matrix(inBytes + rxs->csi_pos, rxs->rxs_basic.nrx, rxs->rxs_basic.ntx, rxs->rxs_basic.num_tones,
+                            rxs->csi_matrix);
+        else
+            iwl_parse_csi_matrix(inBytes + rxs->csi_pos, rxs->rxs_basic.nrx, rxs->rxs_basic.ntx, rxs->rxs_basic.num_tones,
+                                rxs->csi_matrix);
+
+        auto new_tones_num = phaseUnwrapAroundDC(rxs->csi_matrix, rxs->unwrappedMag, rxs->unwrappedPhase, rxs->rxs_basic.nrx, rxs->rxs_basic.ntx, rxs->rxs_basic.num_tones);
+        rxs->rxs_basic.num_tones = new_tones_num;
+    }
+    pos += rxs->rxs_basic.csi_len;
+
+    rxs->payloadLength = totalLength - pos + 2;
+    rxs->txDuration = pkt_duration(rxs->payloadLength, rxs->rxs_basic.rate, rxs->rxs_basic.chanBW, rxs->rxs_basic.sgi);
+
+    uint8_t frameType = *((uint8_t *)(inBytes+pos));
+    if (frameType == 0x8) { // Frame injected by PicoScenes
+        auto packetHeader = (struct ieee80211_packet_header *)(inBytes + pos);
+        rxs->txHeader = * packetHeader;
+        pos += sizeof(struct ieee80211_packet_header);
+
+        if ( packetHeader->header_info.hasTxExtraInfo ) {
+            ExtraInfo::fromBinary(inBytes+pos, &rxs->txExtraInfo); pos += rxs->txExtraInfo.length + 6;
+        }
+
+        if (packetHeader->header_info.hasEchoProbeInfo) {
+            rxs->echoProbeInfo = *((struct EchoProbeInfo *)(inBytes + pos)); pos += sizeof(struct EchoProbeInfo);
+            if (rxs->echoProbeInfo.TxRXSLength > 0) {
+                memcpy(rxs->chronosACKBody, inBytes + pos, rxs->echoProbeInfo.TxRXSLength);
+            }
+            pos += rxs->echoProbeInfo.TxRXSLength;
+        }
+    }
+
+    assert(pos == totalLength);
+    rxs->rawBufferLength = static_cast<uint16_t>(pos);
+    memcpy(rxs->rawBuffer, inBytes, rxs->rawBufferLength);
+    rxs->parsingLevel = parsingLevel;
+
+    return 0;
+}
+
+void inplaceAddTxChronosData(RXS_enhanced * rxs, uint8_t *dataBuffer, int bufferLength) {
+    auto * rawBuffer = rxs->rawBuffer;
+    auto * payloadLength_ptr = (uint16_t *)(rawBuffer+rxs->csi_pos - 2);
+    assert(rxs->payloadLength == *payloadLength_ptr);
+
+    auto headerPos = rxs->csi_pos + rxs->rxs_basic.csi_len;
+    auto * packetHeader_ptr = (struct ieee80211_packet_header *)(rawBuffer + headerPos);
+    assert(rxs->txHeader.header_info.taskId == packetHeader_ptr->header_info.taskId);
+
+    auto chronosPos = headerPos + sizeof(struct ieee80211_packet_header);
+    if (packetHeader_ptr->header_info.hasTxExtraInfo) {
+        auto * txExtraInfoLength_ptr = (uint16_t *)(rawBuffer+chronosPos + 4);
+        chronosPos += *txExtraInfoLength_ptr;
+        chronosPos += 6;
+    }
+    auto * chronos_ptr = (struct EchoProbeInfo *)(rawBuffer + chronosPos);
+    if(!packetHeader_ptr->header_info.hasEchoProbeInfo) {
+        packetHeader_ptr->header_info.hasEchoProbeInfo |= 1;
+        *chronos_ptr = {};
+        chronos_ptr->length = sizeof(struct EchoProbeInfo);
+        chronos_ptr->version = 2;
+
+        rxs->txHeader.header_info.hasEchoProbeInfo |= 1;
+        rxs->echoProbeInfo = *chronos_ptr;
+        rxs->payloadLength += sizeof(struct EchoProbeInfo);
+        rxs->rawBufferLength += sizeof(struct EchoProbeInfo);
+    }
+
+    assert(chronos_ptr->TxRXSLength == rxs->echoProbeInfo.TxRXSLength);
+    auto * chronos_rxs_ptr = rawBuffer + chronosPos + sizeof(struct EchoProbeInfo) + chronos_ptr->TxRXSLength;
+    memcpy(chronos_rxs_ptr, dataBuffer, bufferLength);
+    memcpy(rxs->chronosACKBody + rxs->echoProbeInfo.TxRXSLength, dataBuffer, bufferLength);
+    chronos_ptr->TxRXSLength += bufferLength;
+    *payloadLength_ptr += bufferLength;
+    rxs->rawBufferLength += bufferLength;
+    rxs->payloadLength += bufferLength;
+    rxs->echoProbeInfo.TxRXSLength += bufferLength;
+}
