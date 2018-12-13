e5f9b225820b32eda58b0576876a499c608d0048
Zhiping Jiang <zpj@xidian.edu.cn>
2018-12-14 01:30:04 +0800

    slight optimize iwl parsing

--- IWL_CSIMatrixParser.cxx
+++ IWL_CSIMatrixParser.cxx
@@ -7,7 +7,7 @@
     #include "mex.h"
 #endif
 
-static int positionComputationWRTPermutation(int ntx, int nrx, int nltf, int num_tones, int nrxIdx, int nltfIdx, int ntxIdx, int subcarrierIdx, const struct ExtraInfo & rxExtraInfo) {
+static int positionComputationWRTPermutation(int nrx, int nltf, int num_tones, int ntxIdx, int nrxIdx, int nltfIdx, int subcarrierIdx, const struct ExtraInfo & rxExtraInfo) {
     auto new_nrxIdx = nrxIdx;
     if (rxExtraInfo.hasAntennaSelection == true && nrx > 1) {
         std::vector<int> ant_sel_values(std::begin(rxExtraInfo.ant_sel), std::begin(rxExtraInfo.ant_sel) + nrx);
@@ -16,7 +16,7 @@ static int positionComputationWRTPermutation(int ntx, int nrx, int nltf, int num
         new_nrxIdx = sorted_indexes[nrxIdx];
     }
 
-    return ntxIdx * (nrx * nltf * num_tones) + nltfIdx * (nrx * num_tones) + new_nrxIdx * num_tones + subcarrierIdx;
+    return ntxIdx * (nltf * nrx * num_tones) + nltfIdx * (nrx * num_tones) + new_nrxIdx * num_tones + subcarrierIdx;
 }
 
 void iwl_parse_csi_matrix(const uint8_t *payload, int ntx, int nrx, int nltf, int num_tones, const struct ExtraInfo & rxExtraInfo, std::complex<double> *csi_matrix) {
@@ -29,7 +29,7 @@ void iwl_parse_csi_matrix(const uint8_t *payload, int ntx, int nrx, int nltf, in
         for (auto nrxIdx = 0 ; nrxIdx < nrx ; nrxIdx ++ )
             for (auto nltfIdx = 0; nltfIdx < nltf; nltfIdx ++)
                 for(auto ntxIdx = 0 ; ntxIdx < ntx ; ntxIdx ++ ) {
-                    auto position = positionComputationWRTPermutation(ntx, nrx, nltf, num_tones, nrxIdx, nltfIdx, ntxIdx, subcarrierIdx, rxExtraInfo);
+                    auto position = positionComputationWRTPermutation(nrx, nltf, num_tones, ntxIdx, nrxIdx, nltfIdx, subcarrierIdx, rxExtraInfo);
                     char tmp = (payload[index / 8] >> remainder) |
                           (payload[index/8+1] << (8-remainder));
                     csi_matrix[position].real((double)tmp);
