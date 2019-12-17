2ebb03b3ad85963749a70db24a5cf5b22dee26db
Zhiping Jiang <zpj@xidian.edu.cn>
2019-12-17 23:29:08 +0800

    finish fromBuffer for VariablePicoScenesFrame

--- VariablePicoScenesFrame.cxx
+++ VariablePicoScenesFrame.cxx
@@ -3,3 +3,94 @@
 //
 
 #include "VariablePicoScenesFrame.hxx"
+
+
+bool PicoScenesRxFrameStructure::isOldRXSEnhancedFrame(const uint8_t bufferHead[6]) {
+    auto magicValue = *((uint32_t *) (bufferHead + 2));
+    return (magicValue == 0x20150315 || magicValue == 0x20120930);
+}
+
+std::optional<PicoScenesFrameHeader> PicoScenesFrameHeader::fromBuffer(const uint8_t *buffer) {
+    auto magicValue = *((uint32_t *) (buffer));
+    if (magicValue == 0x20150315 || magicValue == 0x20120930) {
+        auto frameHeader = *((PicoScenesFrameHeader *) (buffer));
+        return frameHeader;
+    }
+    return std::nullopt;
+}
+
+std::optional<PicoScenesRxFrameStructure> PicoScenesRxFrameStructure::fromBuffer(const uint8_t *buffer, uint32_t start, enum RXSParsingLevel parsingLevel) {
+    uint16_t totalLength = 2;
+    uint16_t pos = start;
+    PicoScenesRxFrameStructure rxFrame;
+    totalLength += *((uint16_t *) (buffer + pos));
+    pos += 2;
+
+    rxFrame.rxs_basic = *((struct rx_status_basic *) (buffer + pos));
+    pos += sizeof(struct rx_status_basic);
+    if (rxFrame.rxs_basic.nrx <= 0 || rxFrame.rxs_basic.nrx > 3 || rxFrame.rxs_basic.ntx <= 0 || rxFrame.rxs_basic.ntx > 3) {
+        printf("RXS Parser: Impossible values in nrx (%d), ntx (%d), or num_tones (%d). Error occurs in file format or parsing.\n", rxFrame.rxs_basic.nrx, rxFrame.rxs_basic.ntx, rxFrame.rxs_basic.num_tones);
+        return std::nullopt;
+    }
+
+    ExtraInfo::fromBinary(buffer + pos, &rxFrame.rxExtraInfo);
+    pos += rxFrame.rxExtraInfo.length + 2;
+    if (rxFrame.rxExtraInfo.hasEVM) {
+        for (auto &evm : rxFrame.rxExtraInfo.evm) {
+            evm += rxFrame.rxs_basic.noise;
+            if (evm > 0) {
+                evm = -128;
+            }
+        }
+    }
+
+    if (parsingLevel >= EXTRA_CSI) {
+        if (rxFrame.deviceType == PicoScenesDeviceType::QCA9300)
+            ar_parse_csi_matrix(buffer + pos, rxFrame.rxs_basic.nss / rxFrame.rxs_basic.nrx, rxFrame.rxs_basic.nrx, rxFrame.rxs_basic.num_tones, rxFrame.csi.csi_matrix);
+        else
+            iwl_parse_csi_matrix(buffer + pos, rxFrame.rxs_basic.ntx, rxFrame.rxs_basic.nrx, rxFrame.rxs_basic.nltf, rxFrame.rxs_basic.num_tones, rxFrame.rxExtraInfo, rxFrame.csi.csi_matrix);
+
+        // commit the following two lines to acquire raw csi matrix
+        auto new_tones_num = phaseUnwrapAroundDC(rxFrame.csi.csi_matrix, rxFrame.csi.unwrappedMag, rxFrame.csi.unwrappedPhase, rxFrame.rxs_basic.nss / rxFrame.rxs_basic.nrx, rxFrame.rxs_basic.nrx, rxFrame.rxs_basic.num_tones, rxFrame.rxs_basic.channelBonding);
+        rxFrame.rxs_basic.num_tones = new_tones_num;
+    }
+    pos += rxFrame.rxs_basic.csi_len;
+
+    rxFrame.standardHeader = *((ieee80211_mac_frame_header_frame *) (buffer + pos));
+    pos += sizeof(ieee80211_mac_frame_header_frame);
+
+    rxFrame.PicoScenesHeader = PicoScenesFrameHeader::fromBuffer(buffer + pos);
+    pos += sizeof(PicoScenesFrameHeader);
+
+    if (rxFrame.PicoScenesHeader) {
+        rxFrame.frameSegmentStarts = std::vector<uint16_t>(rxFrame.PicoScenesHeader->segments);
+        rxFrame.segmentMap = std::map<std::string, std::shared_ptr<uint8_t>>();
+        char identifier[2];
+        for (auto i = 0; i < rxFrame.PicoScenesHeader->segments; i++) {
+            identifier[0] = *((char *) (buffer + pos));
+            pos++;
+            identifier[1] = *((char *) (buffer + pos));
+            pos++;
+            auto identifierString = std::string(identifier);
+
+            if (identifierString == "EI") { // Tx ExtraInfo is a special case.
+                ExtraInfo txExtraInfo{};
+                ExtraInfo::fromBinary(buffer + pos, &txExtraInfo);
+                pos += txExtraInfo.length + 2;
+                continue;
+            }
+
+            auto segmentLength = *((uint16_t *) (buffer + pos));
+            auto segmentBuffer = std::shared_ptr<uint8_t>(new uint8_t[segmentLength], std::default_delete<uint8_t[]>());
+            memcpy(segmentBuffer.get(), buffer + pos, segmentLength);
+            pos += segmentLength;
+            rxFrame.segmentMap->emplace(std::make_pair(identifierString, segmentBuffer));
+        }
+    }
+
+    return rxFrame;
+}
+
+int PicoScenesTxFrameStructure::toBuffer(uint8_t *buffer, uint32_t start) {
+    return 0;
+}
--- VariablePicoScenesFrame.hxx
+++ VariablePicoScenesFrame.hxx
@@ -6,6 +6,8 @@
 #define PICOSCENES_PLATFORM_VARIABLEPICOSCENESFRAME_HXX
 
 #include "rxs_enhanced.h"
+#include <optional>
+#include <map>
 
 struct ieee80211_mac_frame_header_frame_control_field {
     uint16_t version         :2,
@@ -21,12 +23,64 @@ struct ieee80211_mac_frame_header_frame_control_field {
             order           :1;
 } __attribute__ ((__packed__));
 
-struct PicoScenesFrameHeader {
-         uint8_t version :4,
-         segments :4;
-         uint8_t frameType;
-         uint16_t taskId;
+struct ieee80211_mac_frame_header_frame {
+    struct ieee80211_mac_frame_header_frame_control_field fc;
+    uint16_t dur;
+    uint8_t addr1[6];
+    uint8_t addr2[6];
+    uint8_t addr3[6];
+    uint16_t seq;
 } __attribute__ ((__packed__));
+
+struct PicoScenesFrameHeader {
+    uint32_t magicValue;
+    uint8_t version :4,
+            segments :4;
+    uint8_t frameType;
+    uint16_t taskId;
+
+    static std::optional<PicoScenesFrameHeader> fromBuffer(const uint8_t *buffer);
+} __attribute__ ((__packed__));
+
+enum class PicoScenesDeviceType : uint16_t {
+    QCA9300 = 9300,
+    IWL5300 = 5300,
+    USRP = 1234,
+};
+
+struct CSIData {
+    std::complex<double> csi_matrix[MAX_OFDM_TONES_UNWRAP];
+    double unwrappedMag[MAX_OFDM_TONES_UNWRAP];
+    double unwrappedPhase[MAX_OFDM_TONES_UNWRAP];
+};
+
+class PicoScenesRxFrameStructure {
+public:
+    PicoScenesDeviceType deviceType;
+    rx_status_basic rxs_basic;
+    ExtraInfo rxExtraInfo;
+    CSIData csi;
+    ieee80211_mac_frame_header_frame standardHeader;
+    std::optional<PicoScenesFrameHeader> PicoScenesHeader;
+    std::optional<std::vector<uint16_t>> frameSegmentStarts;
+    std::optional<ExtraInfo> txExtraInfo;
+    std::optional<std::map<std::string, std::shared_ptr<uint8_t>>> segmentMap;
+
+    static bool isOldRXSEnhancedFrame(const uint8_t bufferHead[6]);
+
+    static std::optional<PicoScenesRxFrameStructure> fromBuffer(const uint8_t *buffer, uint32_t start = 0, enum RXSParsingLevel parsingLevel = RXSParsingLevel::EXTRA_NOCSI);
+};
+
+class PicoScenesTxFrameStructure {
+public:
+    PicoScenesDeviceType deviceType;
+    ieee80211_mac_frame_header_frame standardHeader;
+    PicoScenesFrameHeader frameHeader;
+    std::optional<ExtraInfo> extraInfo;
+    std::map<std::string, std::shared_ptr<uint8_t>> segmentMap;
+
+    static int toBuffer(uint8_t *buffer, uint32_t start = 0);
+};
 
 
 #endif //PICOSCENES_PLATFORM_VARIABLEPICOSCENESFRAME_HXX
