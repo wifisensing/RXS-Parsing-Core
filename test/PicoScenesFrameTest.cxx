116fe16ef5652e95e38aca2831e7ba76091e93fe
Zhiping Jiang <jiangzp.cs@gmail.com>
2019-12-18 14:43:52 +0800

    toBuffer not done.

---
+++ test/PicoScenesFrameTest.cxx
@@ -0,0 +1,15 @@
+//
+// Created by 蒋志平 on 2019/12/18.
+//
+
+#include "gtest/gtest.h"
+#include "../VariablePicoScenesFrame.hxx"
+
+
+namespace PicoScenesFrameTest {
+    TEST(FrameStructure, Tx) {
+        PicoScenesTxFrameStructure txFrameStructure;
+        ExtraInfo txExtraInfo;
+        return;
+    }
+}
