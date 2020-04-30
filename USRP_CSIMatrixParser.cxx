71775bfae6dc1d29629ecc9e9aaf89667fe88299
Zhiping Jiang <jiangzp.cs@gmail.com>
2020-04-30 12:39:14 +0800

    change USRP's CSI matrix output to type float.

--- USRP_CSIMatrixParser.cxx
+++ USRP_CSIMatrixParser.cxx
@@ -5,5 +5,6 @@
 #include "CSIMatrixParser.h"
 
 void usrp_parse_csi_matrix(const uint8_t *csi_addr, int ntx, int nrx, int num_tones, std::complex<double> *csi_matrix) {
-    memcpy(csi_matrix, csi_addr, ntx * nrx * num_tones * sizeof(std::complex<double>));
+    for (auto i = 0; i < ntx * nrx * num_tones; i++)
+        csi_matrix[i] = std::complex<double>(((std::complex<float> *) csi_addr + i)->real(), ((std::complex<float> *) csi_addr + i)->imag());
 }
