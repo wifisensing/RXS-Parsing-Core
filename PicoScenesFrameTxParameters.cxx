eef4beb88a5a98fc1ab8a877851b74488d0b920c
renxincheng <13323919496@163.com>
2020-11-25 20:13:18 +0800

    fix bug display mcs

--- PicoScenesFrameTxParameters.cxx
+++ PicoScenesFrameTxParameters.cxx
@@ -7,7 +7,7 @@
 
 std::string PicoScenesFrameTxParameters::toString() const {
     std::stringstream ss;
-    ss << "tx_param[mcs=" << mcs[0] << ", cbw=" << ChannelBandwidth2String(cbw) << ", gi=" << GuardInterval2String(guardInterval) << ", sounding =" << forceSounding << ", NESS=" << int(numExtraSounding) << ", coding=" << ChannelCoding2String(coding[0]) << "]";
+    ss << "tx_param[mcs=" << std::to_string(mcs[0]) << ", cbw=" << ChannelBandwidth2String(cbw) << ", gi=" << GuardInterval2String(guardInterval) << ", sounding =" << forceSounding << ", NESS=" << int(numExtraSounding) << ", coding=" << ChannelCoding2String(coding[0]) << "]";
     return ss.str();
 }
 
