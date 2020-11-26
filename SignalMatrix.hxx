8c6f4eec158119b787614c2ace645070a1531733
Zhiping Jiang <zpj@xidian.edu.cn>
2020-11-27 04:57:17 +0800

    add SignalMatrix toBuffer method

--- SignalMatrix.hxx
+++ SignalMatrix.hxx
@@ -12,12 +12,27 @@
 #include <iterator>
 #include <string>
 #include <cstring>
+#include <numeric>
 
 enum class SignalMatrixStorageMajority : char {
     RowMajor = 'R',
     ColumnMajor = 'C',
-    UndefinedMajority = 'U'
+    UndefinedMajority = 'U',
 };
+
+//std::ostream &operator<<(std::ostream &os, SignalMatrixStorageMajority majority) {
+//    switch (majority) {
+//        case SignalMatrixStorageMajority::RowMajor :
+//            return os << "ARP";
+//        case SignalMatrixStorageMajority::ColumnMajor:
+//            return os << "IPv4";
+//        case SignalMatrixStorageMajority::UndefinedMajority:
+//            return os << "VLAN";
+//        case SignalMatrixStorageMajority::Default:
+//            return os << "IPv6";
+//    };
+//    return os << static_cast<uint8_t>(majority);
+//}
 
 template<typename>
 struct is_std_complex : std::false_type {
@@ -34,12 +49,12 @@ public:
 
     std::vector<InputType> array;
     std::vector<int32_t> dimensions;
-    SignalMatrixStorageMajority major = SignalMatrixStorageMajority::UndefinedMajority;
+    SignalMatrixStorageMajority majority = SignalMatrixStorageMajority::UndefinedMajority;
 
     SignalMatrix() = default;
 
     template<typename DimensionContainerType>
-    SignalMatrix(const std::vector<InputType> &array, const DimensionContainerType &dimensionsV, SignalMatrixStorageMajority majority) : array(array), major(majority) {
+    SignalMatrix(const std::vector<InputType> &array, const DimensionContainerType &dimensionsV, SignalMatrixStorageMajority majority = SignalMatrixStorageMajority::UndefinedMajority) : array(array), majority(majority) {
         for (auto it = std::cbegin(dimensionsV); it != std::cend(dimensionsV); it++) {
             dimensions.emplace_back(*it);
         }
@@ -49,6 +64,84 @@ public:
 
         if (sum != array.size())
             throw std::invalid_argument("SignalMatrix creation failed due to the inconsistent dimensions.");
+    }
+
+    std::vector<uint8_t> toBuffer(SignalMatrixStorageMajority outputMajority = SignalMatrixStorageMajority::UndefinedMajority) {
+        auto getTypeChar = [] {
+            if (std::is_same_v<ElementType, float>) { return 'F'; }
+            else if (std::is_same_v<ElementType, double>) { return 'D'; }
+            else if (std::is_same_v<ElementType, uint8_t>) { return 'U'; }
+            else if (std::is_same_v<ElementType, uint16_t>) { return 'U'; }
+            else if (std::is_same_v<ElementType, uint32_t>) { return 'U'; }
+            else if (std::is_same_v<ElementType, uint64_t>) { return 'U'; }
+            else if (std::is_same_v<ElementType, int8_t>) { return 'I'; }
+            else if (std::is_same_v<ElementType, int16_t>) { return 'I'; }
+            else if (std::is_same_v<ElementType, int32_t>) { return 'I'; }
+            else if (std::is_same_v<ElementType, int64_t>) { return 'I'; }
+            else if (std::is_same_v<ElementType, char>) { return 'C'; }
+            else if (std::is_same_v<ElementType, bool>) { return 'L'; }
+            throw std::runtime_error("BBSignalsFileWriter: unsupported element type.");
+        };
+
+        auto computePositionUnderInversedMajority = [&](uint32_t newPos) -> uint32_t {
+            auto revD = dimensions;
+            std::reverse(revD.begin(), revD.end());
+            std::vector<uint32_t> coordinates(revD.size(), 0);
+            auto inputPos = newPos;
+            for (auto i = 1; i <= revD.size(); i++) {
+                auto numElementsPerDimension = std::accumulate(revD.cbegin(), revD.cend() - i, 1, std::multiplies<>());
+                coordinates[dimensions.size() - i] = inputPos / numElementsPerDimension;
+                inputPos %= numElementsPerDimension;
+            }
+            auto oldCoordinates = coordinates;
+            std::reverse(oldCoordinates.begin(), oldCoordinates.end());
+
+            auto oldPos = 0;
+            for (auto i = 0; i < coordinates.size(); i++) {
+                auto numElementsPerDimension = std::accumulate(dimensions.cbegin(), dimensions.cbegin() + i, 1, std::multiplies<>());
+                oldPos += numElementsPerDimension * oldCoordinates[i];
+            }
+
+            return oldPos;
+        };
+
+        std::vector<uint8_t> vout;
+        vout.reserve(array.size() * sizeof(InputType) + 50);
+        std::string header_version("BBv1");
+        std::copy(header_version.cbegin(), header_version.cend(), std::back_inserter(vout));
+        vout.emplace_back(dimensions.size());
+        for (const auto &dimension: dimensions) {
+            std::copy((uint8_t *) &dimension, ((uint8_t *) &dimension) + sizeof(dimension), std::back_inserter(vout));
+        }
+
+        vout.emplace_back(is_std_complex<InputType>::value ? 'C' : 'R');
+        vout.emplace_back(getTypeChar());
+        vout.emplace_back(sizeof(ElementType) * 8);
+
+        outputMajority = outputMajority == SignalMatrixStorageMajority::UndefinedMajority ? majority : outputMajority;
+        switch (outputMajority) {
+            case SignalMatrixStorageMajority::RowMajor:
+                vout.emplace_back('R');
+                break;
+            case SignalMatrixStorageMajority::ColumnMajor:
+                vout.emplace_back('C');
+                break;
+            case SignalMatrixStorageMajority::UndefinedMajority:
+                throw std::runtime_error("SignalMatrix::toBuffer failed due to unsupported storage majority.");
+        }
+
+        if (majority == outputMajority) {
+            for (const auto &value : array) {
+                std::copy((uint8_t *) &value, (uint8_t *) &value + sizeof(InputType), std::back_inserter(vout));
+            }
+        } else {
+            for (auto i = 0; i < array.size(); i++) {
+                auto pos = computePositionUnderInversedMajority(i);
+                std::copy((uint8_t *) &array[pos], ((uint8_t *) &array[pos]) + sizeof(InputType), std::back_inserter(vout));
+            }
+        }
+
+        return vout;
     }
 
 
--- test/PicoScenesFrameTest.cxx
+++ test/PicoScenesFrameTest.cxx
@@ -8,8 +8,13 @@
 
 namespace PicoScenesFrameTest {
     TEST(ExtraInfo, IO) {
-        std::vector<int8_t> a = {1, 2, 3, 45, 6, 7, 8, 9};
-        std::string ab("BBv1");
-        SignalMatrix<double>::loadFromBuffer(&a[0], &a[4]);
+        std::vector<int8_t> a;
+        for (auto i = 0; i < 24; i++)
+            a.emplace_back(i);
+        SignalMatrix sm(a, std::vector<int16_t>{4, 3, 2}, SignalMatrixStorageMajority::RowMajor);
+        auto vout = sm.toBuffer();
+        auto vout2 = sm.toBuffer(SignalMatrixStorageMajority::RowMajor);
+        auto vout3 = sm.toBuffer(SignalMatrixStorageMajority::ColumnMajor);
+        auto vout4 = sm.toBuffer(SignalMatrixStorageMajority::ColumnMajor);
     }
 }
