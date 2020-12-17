7cda9a02faf23362237a4026c031dbb1cf74ad15
Zhiping Jiang <zpj@xidian.edu.cn>
2020-12-17 16:11:45 +0800

    updating CSISegment

--- CSISegment.cxx
+++ CSISegment.cxx
@@ -156,7 +156,7 @@ void CSI::interpolateCSI() {
     coder::array<short, 1U> interpedIndex_int16;
     coder::array<short, 1U> subcarrierIndex_int16;
 
-    CSI.set_size(dimensions.numTones, dimensions.numTx * dimensions.numRx);
+    CSI.set_size(dimensions.numTones, (dimensions.numTx + dimensions.numESS) * dimensions.numRx);
     for (auto toneIndex = 0; toneIndex < CSI.size(0); toneIndex++) {
         for (auto txTrIndex = 0; txTrIndex < CSI.size(1); txTrIndex++) {
             auto pos = toneIndex + txTrIndex * CSI.size(0);
@@ -318,7 +318,7 @@ CSISegment CSISegment::createByBuffer(const uint8_t *buffer, uint32_t bufferLeng
 std::string CSISegment::toString() const {
     std::stringstream ss;
     ss << segmentName + ":[";
-    ss << "(device=" << csi.deviceType << ", format=" << csi.packetFormat << ", CBW=" << csi.cbw << ", dim(nTones,nSTS,nESS,nRx)=(" + std::to_string(csi.dimensions.numTones) + "," + std::to_string(csi.dimensions.numTx) + "," + std::to_string(csi.dimensions.numESS) + "," + std::to_string(csi.dimensions.numRx) + "), raw=" + std::to_string(csi.rawCSIData.size()) + "B)]";
+    ss << "(device=" << csi.deviceType << ", format=" << csi.packetFormat << ", CBW=" << csi.cbw << ", cf=" << std::to_string((double)csi.carrierFreq / 1e6) << "MHz" << ", sf=" << std::to_string((double)csi.samplingRate / 1e6) << "MHz" ", subcarrierBW=" << std::to_string((double)csi.subcarrierBandwidth / 1e3) << "KHz" << ", dim(nTones,nSTS,nESS,nRx)=(" + std::to_string(csi.dimensions.numTones) + "," + std::to_string(csi.dimensions.numTx) + "," + std::to_string(csi.dimensions.numESS) + "," + std::to_string(csi.dimensions.numRx) + "), raw=" + std::to_string(csi.rawCSIData.size()) + "B)]";
     auto temp = ss.str();
     temp.erase(temp.end() - 2, temp.end());
     temp.append("]");
