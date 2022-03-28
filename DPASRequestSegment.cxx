0e2da986ccc2d9439c0fc4ae8db1abefc372516b
Zhiping Jiang <zpj@xidian.edu.cn>
2022-03-29 07:50:53 +0800

    Fixing compiliation bug

--- DPASRequestSegment.cxx
+++ DPASRequestSegment.cxx
@@ -241,7 +241,7 @@ void DPASRequestSegment::fromBuffer(const uint8_t *buffer, uint32_t bufferLength
         throw std::runtime_error("DPASRequestSegment cannot parse the segment named " + segmentName + ".");
     if (segmentLength + 4 > bufferLength)
         throw std::underflow_error("DPASRequestSegment cannot parse the segment with less than " + std::to_string(segmentLength + 4) + "B.");
-    if (!versionedSolutionMap.contains(versionId)) {
+    if (!versionedSolutionMap.count(versionId)) {
         throw std::runtime_error("DPASRequestSegment cannot parse the segment with version v" + std::to_string(versionId) + ".");
     }
 
