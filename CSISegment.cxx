c925c35d0984c42b1f7953d24ad46feb82407707
Zhiping Jiang <zpj@xidian.edu.cn>
2023-08-20 23:31:12 +0800

    Improve CSI segment display

--- CSISegment.cxx
+++ CSISegment.cxx
@@ -730,7 +730,7 @@ void CSISegment::setCSI(const CSI &csiV) {
 std::string CSISegment::toString() const {
     std::stringstream ss;
     ss << segmentName + ":[";
-    ss << "device=" << csi.deviceType << ", format=" << csi.packetFormat << ", CBW=" << csi.cbw << ", cf=" << std::to_string((double) csi.carrierFreq / 1e6) << " MHz" << ", sf=" << std::to_string((double) csi.samplingRate / 1e6) << " MHz" ", subcarrierBW=" << std::to_string((double) csi.subcarrierBandwidth / 1e3) << " kHz"
+    ss << "device=" << csi.deviceType << ", format=" << csi.packetFormat << ", CBW=" << csi.cbw << ", cf=" << std::to_string((double) csi.carrierFreq / 1e6) << " MHz" << (csi.isMerged ? ", cf2=" + std::to_string((double) csi.carrierFreq2 / 1e6) + " MHz" : "") << ", sf=" << std::to_string((double) csi.samplingRate / 1e6) << " MHz" ", subcarrierBW=" << std::to_string((double) csi.subcarrierBandwidth / 1e3) << " kHz"
        << ", dim(nTones,nSTS,nESS,nRx,nCSI)=(" + std::to_string(csi.dimensions.numTones) + "," + std::to_string(csi.dimensions.numTx) + "," + std::to_string(csi.dimensions.numESS) + "," + std::to_string(csi.dimensions.numRx) + "," + std::to_string(csi.dimensions.numCSI) + "), raw=" + std::to_string(csi.rawCSIData.size()) + "B]";
     auto temp = ss.str();
     temp.erase(temp.end() - 2, temp.end());
